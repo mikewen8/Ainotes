71e3fe97466137b8c7f1db02103bfe2544922a0f
mikewen8 <Mikecwen@gmail.com>
2024-04-28 23:12:54 -0700

    fixed the setup for mongo

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 
 from pymongo.mongo_client import MongoClient
 
-uri = "mongodb+srv://Mike:<Mikedev88>@studygroup.xxnpe30.mongodb.net/?retryWrites=true&w=majority&appName=Studygroup"
+uri = "mongodb+srv://Mike:Mikedev88@studygroup.xxnpe30.mongodb.net/?retryWrites=true&w=majority&appName=Studygroup"
 
 # Create a new client and connect to the server
 client = MongoClient(uri)
