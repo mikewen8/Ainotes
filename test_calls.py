d665cd95bf4f69b6b022fbaf2f354e0e8da05671
mikewen8 <mikecwen@gmail.com>
2024-04-30 14:08:57 -0700

    variable calling

--- test_calls.py
+++ test_calls.py
@@ -1,8 +1,10 @@
 from doc_create import add_doc
 
+int (id)
+
 
 document = {
-    "_id": 1,
+    "_id": id,
     "Name": "Mike",
     "Note": "This is the very first note and it is a very very long note, it can be a very long long note maybe a full essay woth of notes I'm trying to see how much storage this will take for this super super long note",
 }
