8b7431b0031410896b944c8409c5beb49fcb1c78
mikewen8 <mikecwen@gmail.com>
2024-04-30 14:22:06 -0700

    created a function for adding a new doc

--- test_calls.py
+++ test_calls.py
@@ -1,12 +1,14 @@
 from doc_create import add_doc
+from pull import pull_doc, pull_user
+id = pull_user()
 
-int (id)
-
-
+new_id =id["_id"]
+new_id +=1
+print(new_id)
 document = {
-    "_id": id,
+    "_id": new_id,
     "Name": "Mike",
     "Note": "This is the very first note and it is a very very long note, it can be a very long long note maybe a full essay woth of notes I'm trying to see how much storage this will take for this super super long note",
 }
 
-add_doc(document)
+#add_doc(document)
