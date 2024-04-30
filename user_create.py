00df041f323534bbaccce4055b7d5fbdb13bd763
mikewen8 <mikecwen@gmail.com>
2024-04-30 13:20:09 -0700

    create a user in the db

---
+++ user_create.py
@@ -0,0 +1,26 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://Mike:Mikedev88@studygroup.zvgkeux.mongodb.net/?retryWrites=true&w=majority&appName=Studygroup"
+
+# Create a new client and connect to the server
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+# Select the database
+db = client['Studygroup']
+
+# Select the collection
+collection = db['Users']
+
+# Document to insert
+document = {
+    "userid": 1,
+    "Name": "Mike"
+}
+
+# Insert the document into the collection
+try:
+    result = collection.insert_one(document)
+    print("Inserted document id:", result.inserted_id)
+except Exception as e:
+    print("An error occurred:", e)
