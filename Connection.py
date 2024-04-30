965304749b688a8eb25b2fe046bd2656f0e6cfb9
mikewen8 <mikecwen@gmail.com>
2024-04-30 13:40:06 -0700

    changed

--- Connection.py
+++ Connection.py
@@ -1,7 +1,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://<username>:<password>@studygroup.zvgkeux.mongodb.net/?retryWrites=true&w=majority&appName=Studygroup"
+uri = "mongodb+srv://Mike:Mikedev88studygroup.zvgkeux.mongodb.net/?retryWrites=true&w=majority&appName=Studygroup"
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
