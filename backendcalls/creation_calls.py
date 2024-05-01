799fae3bc9493f34f536690082427c60db99e74d
mikewen8 <mikecwen@gmail.com>
2024-04-30 18:56:25 -0700

    added pswds

--- backendcalls/creation_calls.py
+++ backendcalls/creation_calls.py
@@ -3,7 +3,7 @@ from user_create import add_user
 from pull import pull_doc, pull_user
 id = pull_user()
 doc =pull_doc()
-
+#we need to get the json from flask
 new_doc=doc["_id"]
 new_doc+=1
 print(new_doc)
@@ -15,14 +15,15 @@ new_document = {
     "Note": "This is the very first note and it is a very very long note, it can be a very long long note maybe a full essay woth of notes I'm trying to see how much storage this will take for this super super long note",
 }
 
-
+#need to create a new passowrd
 new_id =id["_id"]
 new_id +=1
 print(new_id)
 name = input(" input your name")
 new_user ={
     "_id": new_id,
-    "Name":name
+    "Name":name,
+    "Pswd":"pswd"
 }
 
 def create_doc(new_document):
