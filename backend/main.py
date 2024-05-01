2523b2b3a781c8da906f5b61559136d86ed2689b
mikewen8 <mikecwen@gmail.com>
2024-04-30 23:18:51 -0700

    v1

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import request, jsonify
 from flask_cors import CORS
-from backendcalls import creation_calls
+from backendcalls import creation_calls,pull
 import os
 
 app = Flask(__name__)
@@ -12,10 +12,16 @@ app.config["CORS_HEADERS"]= "Content-Type"
 
 app.route('/Pull',methods =["Post","GET"])
 def Pull():
-    content = request.args.get("name")
-    creation_calls.create_user("name")
+    content = request.args.get("Name")
+    pull.pull_user(content)
     #here I need to do the pymongo here 
-    
+    return jsonify({"message": "Data pulled successfully"})
+
+
+app.route('/Pull',methods =["Post","GET"])
+def Push():   
+    content = request.get_json()
+    creation_calls.create_user(content)
 
 if __name__ == "__main__":
     app.run()
--- backendcalls/creation_calls.py
+++ backendcalls/creation_calls.py
@@ -27,25 +27,9 @@ new_user ={
 }
 
 def create_doc(new_document):
-    doc =pull_doc()
-    new_doc=doc["_id"]
-    new_doc+=1
-    new_document = {
-    "_id": new_doc,
-    "userid":id["_id"],
-    "Name": "Mike",
-    "Note": "This is the very first note and it is a very very long note, it can be a very long long note maybe a full essay woth of notes I'm trying to see how much storage this will take for this super super long note",
-    }
     add_doc(new_document)
-
-def create_user(username):
-    id = pull_user()
-    new_id =id["_id"]
-    new_id +=1
-    new_user ={
-    "_id": new_id,
-    "Name":username,
-    "Pswd":"pswd"
-    }
+def create_user(new_user):
     add_user(new_user)
     
+create_doc(new_document)
+create_user(new_user)
