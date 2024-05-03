063c03705b1d00d43728fd566d30f0f7688fbdbe
mikewen8 <mikecwen@gmail.com>
2024-05-02 17:20:02 -0700

    finished and backend commit work chekc

--- flask/app.py
+++ flask/app.py
@@ -65,6 +65,5 @@ def index():
     return render_template('index.html', collection = all_notes)
 
 
-
 if __name__ == '__main__':
-    app.run(host='127.0.0.1',port=5000, debug=True)
+    app.run(debug=True)
